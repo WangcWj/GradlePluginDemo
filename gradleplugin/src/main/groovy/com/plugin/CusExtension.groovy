b0756da1247078ea116521081c6b698d63cac429
Wj_Wang <wangchaochaoj@163.com>
2018-10-25 16:58:29 +0800

    10-25

---
+++ gradleplugin/src/main/groovy/com/plugin/CusExtension.groovy
@@ -0,0 +1,12 @@
+
+package com.plugin
+
+class CusExtension {
+    String name = ""
+    String path = ""
+
+    @Override
+    String toString() {
+        return "name is  " + name +"     path  is   " +path
+    }
+}
--- gradleplugin/src/main/groovy/com/plugin/DemoPlugin.groovy
+++ gradleplugin/src/main/groovy/com/plugin/DemoPlugin.groovy
@@ -3,11 +3,13 @@ package com.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+
 class DemoPlugin implements Plugin<Project>{
     @Override
     void apply(Project project) {
-        println("====================")
-        println("插件搭载成功了啊!")
-        println("====================")
+        project.extensions.create("cus",CusExtension)
+        project.afterEvaluate {
+            println("=================   "+project.cus.name)
+        }
     }
 }
